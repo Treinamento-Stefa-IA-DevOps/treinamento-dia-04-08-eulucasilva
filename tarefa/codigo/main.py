9710dafa572d473353ef8fc8f4360609a818121f
Lucas Silva <lucassilvasistemas@gmail.com>
2021-08-05 20:49:04 -0300

    Adicionando código para criação de API de predição de sobrevivência Titanic

--- tarefa/codigo/main.py
+++ tarefa/codigo/main.py
@@ -4,9 +4,10 @@ from fastapi import FastAPI
 app = FastAPI()
 @app.post('/model')
 ## Coloque seu codigo na função abaixo
-def titanic(Sex:int):
+def titanic(Sex:int, Age:float, Lifeboat:int, Pclass: int):
     with open('model/Titanic.pkl', 'rb') as fid: 
         titanic = pickle.load(fid)
+    return titanic
 
 @app.get('/model')
 def get():
