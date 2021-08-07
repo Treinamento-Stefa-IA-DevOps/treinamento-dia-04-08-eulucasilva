58ab6668447003ec87ef934fdcef082c101efd49
Lucas Silva <lucassilvasistemas@gmail.com>
2021-08-06 22:33:53 -0300

    Código finalizado. Rodando localmente

--- tarefa/codigo/main.py
+++ tarefa/codigo/main.py
@@ -1,5 +1,5 @@
 import pickle
-from fastapi import FastAPI, status
+from fastapi import FastAPI, Query
 
 app = FastAPI(
     title = "API Sobrevivência Titanic",
@@ -10,15 +10,23 @@ app = FastAPI(
 
 @app.post('/model', status_code=200)
 ## Coloque seu codigo na função abaixo
-def titanic(Sex:int, Age:float, Lifeboat:int, Pclass: int):
+def titanic(*, Sex:int = Query(..., ge=0, le=1), Age:float = Query(..., gt=0.1), Lifeboat:int, Pclass: int = Query(..., ge=1, le=3)):
     with open('model/Titanic.pkl', 'rb') as fid: 
         titanic = pickle.load(fid)
-        predicao = titanic.predict([[Sex, Age, Lifeboat, Pclass]]).tolist() 
-       
+    
+    predicao = titanic.predict([[Sex, Age, Lifeboat, Pclass]]) 
+
+    #print (predicao)
+    survivor = bool(predicao)
+    #print (survivor)
+    mensagem = "Você morreria no Titanic!"
+    if survivor:
+        mensagem = "Predição realizada com sucesso: Você sobreviveria ao Titanic. Ufa!"
+
     return {
-        'survived': predicao,
-        'status': status.HTTP_200_CREATED,
-        'message': "Predição realizada com sucesso: Você sobreviveria ao Titanic. Ufa!"
+        'survived': survivor,
+        'status': 200,
+        'message': mensagem
     }
 
 @app.get('/model')
