924dd011aee6ba036dff0c56638d4d799b2389b0
Lucas Silva <lucassilvasistemas@gmail.com>
2021-08-06 21:22:32 -0300

    Alterando código para correção do retorno

--- tarefa/codigo/main.py
+++ tarefa/codigo/main.py
@@ -1,14 +1,26 @@
 import pickle
-from fastapi import FastAPI
+from fastapi import FastAPI, status
 
-app = FastAPI()
-@app.post('/model')
+app = FastAPI(
+    title = "API Sobrevivência Titanic",
+    version = "1.0.0",
+    description  = "Prediz se um pessoa com determinadas características passadas como parâmetro é capaz de sobreviver a algum possível acidente do Titanic"
+)
+
+
+@app.post('/model', status_code=200)
 ## Coloque seu codigo na função abaixo
 def titanic(Sex:int, Age:float, Lifeboat:int, Pclass: int):
     with open('model/Titanic.pkl', 'rb') as fid: 
         titanic = pickle.load(fid)
-    return titanic
+        predicao = titanic.predict([[Sex, Age, Lifeboat, Pclass]]).tolist() 
+       
+    return {
+        'survived': predicao,
+        'status': status.HTTP_200_CREATED,
+        'message': "Predição realizada com sucesso: Você sobreviveria ao Titanic. Ufa!"
+    }
 
 @app.get('/model')
 def get():
-    return {'hello':'test'}
+    return "API para predição de sobrevivência do Titanic"
